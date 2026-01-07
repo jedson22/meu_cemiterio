c51eecd9f41b47299c9713f4ad19a0d917b6d910
jedson22 <jedson1991cn@gmail.com>
2026-01-06 23:02:52 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-urlpatterns = [ path('admin/', admin.site.urls), path('', include('core.urls')), ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # Sistema de Login/Logout padrão do Django
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+    path('', include('core.urls')),
+]
