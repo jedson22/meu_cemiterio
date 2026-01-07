aa3828cef55b3c5974537f695c40597f14b12285
jedson22 <jedson1991cn@gmail.com>
2026-01-06 22:22:11 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Página inicial
+    # --- PÁGINAS ---
     path('', views.index, name='index'),
-    
-    # Visualizações
     path('quadra/<int:quadra_id>/', views.detalhe_quadra, name='detalhe_quadra'),
     path('lote/<int:q_id>/<int:l_id>/', views.detalhe_lote, name='detalhe_lote'),
     
-    # Ações (Botões)
+    # --- AÇÕES DE LOTE E GAVETA ---
     path('vender-lote/<int:lote_id>/', views.vender_lote, name='vender_lote'),
     path('registrar-obito/<int:gaveta_id>/', views.registrar_obito, name='registrar_obito'),
+    path('limpar-gaveta/<int:gaveta_id>/', views.limpar_gaveta, name='limpar_gaveta'), # Novo
+
+    # --- AÇÕES DE ESTRUTURA (CRIAR/EXCLUIR) ---
+    path('adicionar-quadra/', views.adicionar_quadra, name='adicionar_quadra'),
+    path('excluir-quadra/<int:quadra_id>/', views.excluir_quadra, name='excluir_quadra'),
+    path('adicionar-lote/<int:quadra_id>/', views.adicionar_lote, name='adicionar_lote'),
+    path('excluir-lote/<int:lote_id>/', views.excluir_lote, name='excluir_lote'),
 ]
