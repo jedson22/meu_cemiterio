aef8e7bcea096b2fce5b99e796e1600d16f7e315
jedson22 <jedson1991cn@gmail.com>
2026-01-06 22:11:35 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -3,10 +3,11 @@ from .models import Quadra, Lote
 
 # 1. PÁGINA INICIAL (Mapa Geral)
 def index(request):
+    # Busca todas as quadras e ordena pelo número
     quadras = Quadra.objects.all().order_by('numero')
     return render(request, 'index.html', {'quadras': quadras})
 
-# 2. DETALHES DA QUADRA (Caso você use links diretos pra quadra)
+# 2. DETALHES DA QUADRA (Opcional)
 def detalhe_quadra(request, quadra_id):
     quadra = get_object_or_404(Quadra, id=quadra_id)
     lotes = quadra.lotes.all().order_by('numero')
@@ -14,6 +15,7 @@ def detalhe_quadra(request, quadra_id):
 
 # 3. DETALHES DO LOTE (Ver Gavetas)
 def detalhe_lote(request, q_id, l_id):
+    # Busca o lote pelo número da quadra e número do lote
     lote = get_object_or_404(Lote, quadra__numero=q_id, numero=l_id)
     return render(request, 'detalhe_lote.html', {'lote': lote})
 
@@ -27,5 +29,5 @@ def vender_lote(request, lote_id):
             lote.proprietario = nome
             lote.save()
             
-    # Retorna para a página anterior
+    # Retorna para a página anterior (recarrega o mapa)
     return redirect(request.META.get('HTTP_REFERER', '/'))
