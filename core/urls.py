b282b121916a6bf2844019862c680734fec4b5ca
jedson22 <jedson1991cn@gmail.com>
2026-01-06 21:41:29 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
+    # Home
     path('', views.index, name='index'),
-    path('quadra/<int:q_id>/', views.detalhe_quadra, name='detalhe_quadra'),
+    
+    # Visualizar uma Quadra inteira
+    path('quadra/<int:quadra_id>/', views.detalhe_quadra, name='detalhe_quadra'),
+    
+    # Visualizar um Lote específico (A linha que estava dando erro)
     path('lote/<int:q_id>/<int:l_id>/', views.detalhe_lote, name='detalhe_lote'),
-    path('lote/<int:q_id>/<int:l_id>/excluir/', views.excluir_lote, name='excluir_lote'),
-    path('config/', views.config, name='config'),
-    path('exportar/', views.exportar, name='exportar'),
+
+    # Ação de Vender
     path('vender-lote/<int:lote_id>/', views.vender_lote, name='vender_lote'),
 ]
