eb610d5a9103a980c08208ef1f77fe2b18b7741f
jedson22 <jedson1991cn@gmail.com>
2026-01-06 21:53:42 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -33,17 +33,19 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        # MUDAR AQUI: Adicione o caminho da pasta templates
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug', 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
-
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # --- AQUI ESTÁ A MUDANÇA DO BANCO DE DADOS ---
