261385f9be2e1bb6ca3321b1b4673b690346405e
jedson22 <jedson1991cn@gmail.com>
2026-01-08 19:55:45 -0300

    Create 0004_historico.py

---
+++ core/migrations/0004_historico.py
@@ -0,0 +1,25 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0003_produto'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Historico',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('nome', models.CharField(max_length=200)),
+                ('data_falecimento', models.DateField(blank=True, null=True)),
+                ('data_exumacao', models.DateField(auto_now_add=True)),
+                ('observacao', models.TextField(blank=True, null=True)),
+                ('gaveta', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='historico', to='core.gaveta')),
+            ],
+            options={
+                'ordering': ['-data_exumacao'],
+            },
+        ),
+    ]
