d7b3ead6ae162175f8582ad7e767244db2145e1b
jedson22 <jedson1991cn@gmail.com>
2026-01-08 19:59:34 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,23 +2,27 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Principal
     path('', views.index, name='index'),
     path('sair/', views.encerrar_sessao, name='sair'),
     path('relatorio/', views.gerar_relatorio, name='gerar_relatorio'),
 
-    # Cemitério
     path('quadra/<int:quadra_id>/', views.detalhe_quadra, name='detalhe_quadra'),
     path('lote/<int:q_id>/<int:l_id>/', views.detalhe_lote, name='detalhe_lote'),
+    
     path('vender-lote/<int:lote_id>/', views.vender_lote, name='vender_lote'),
     path('transferir-lote/<int:lote_id>/', views.transferir_lote, name='transferir_lote'),
     path('registrar-obito/<int:gaveta_id>/', views.registrar_obito, name='registrar_obito'),
     path('limpar-gaveta/<int:gaveta_id>/', views.limpar_gaveta, name='limpar_gaveta'),
+    
+    # Histórico Manual
+    path('add-historico/<int:gaveta_id>/', views.adicionar_historico_manual, name='adicionar_historico_manual'),
 
-    # Estrutura
     path('adicionar-quadra/', views.adicionar_quadra, name='adicionar_quadra'),
     path('excluir-quadra/<int:quadra_id>/', views.excluir_quadra, name='excluir_quadra'),
-    path('adicionar-lote/<int:quadra_id>/', views.adicionar_lote, name='adicionar_lote'),
+    
+    # Lote com número
+    path('form-lote/<int:quadra_id>/', views.form_adicionar_lote, name='form_adicionar_lote'),
+    path('salvar-lote/<int:quadra_id>/', views.salvar_lote, name='salvar_lote'),
     path('excluir-lote/<int:lote_id>/', views.excluir_lote, name='excluir_lote'),
 
     # Estoque
