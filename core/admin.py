4943c1e7465c2f93edac53ac3a08aab08afe24b9
jedson22 <jedson1991cn@gmail.com>
2026-01-09 14:42:52 -0300

    Create admin.py

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Quadra, Lote, Gaveta, Falecido
+
+admin.site.register(Quadra)
+admin.site.register(Lote)
+admin.site.register(Gaveta)
+admin.site.register(Falecido)
