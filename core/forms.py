ecd9a13bbb86d41dc367c467533f92fddce11bc9
jedson22 <jedson1991cn@gmail.com>
2026-01-09 16:03:58 -0300

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Lote, Falecido
+from .models import Lote, Falecido, Produto
 
 class LoteForm(forms.ModelForm):
     class Meta:
@@ -11,6 +11,11 @@ class FalecidoForm(forms.ModelForm):
         model = Falecido
         fields = ['nome', 'data_nascimento', 'data_falecimento', 'observacao']
         widgets = {
-            'data_nascimento': forms.DateInput(attrs={'type': 'date'}),
-            'data_falecimento': forms.DateInput(attrs={'type': 'date'}),
+            'data_nascimento': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'data_falecimento': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
         }
+
+class ProdutoForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['nome', 'categoria', 'quantidade', 'preco', 'descricao']
