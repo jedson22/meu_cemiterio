39bd818c29089b8f87535549694ff0b36ba55b5f
jedson22 <jedson1991cn@gmail.com>
2026-01-09 14:43:17 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     path('quadra/<int:quadra_id>/novo_lote/', views.criar_lote, name='criar_lote'),
     path('lote/<int:lote_id>/', views.detalhe_lote, name='detalhe_lote'),
     path('gaveta/<int:gaveta_id>/adicionar/', views.adicionar_falecido, name='adicionar_falecido'),
-    
-    # Rota para o botão de exumar
     path('falecido/<int:falecido_id>/exumar/', views.exumar_falecido, name='exumar_falecido'),
 ]
