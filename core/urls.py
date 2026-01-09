a82f9bab35f767e4a8c1b39977daad5f2bac18c2
jedson22 <jedson1991cn@gmail.com>
2026-01-09 16:04:53 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
+    # Cemitério
     path('quadra/<int:quadra_id>/', views.detalhe_quadra, name='detalhe_quadra'),
     path('quadra/<int:quadra_id>/novo_lote/', views.criar_lote, name='criar_lote'),
     path('lote/<int:lote_id>/', views.detalhe_lote, name='detalhe_lote'),
     path('gaveta/<int:gaveta_id>/adicionar/', views.adicionar_falecido, name='adicionar_falecido'),
     path('falecido/<int:falecido_id>/exumar/', views.exumar_falecido, name='exumar_falecido'),
+    
+    # Estoque
+    path('estoque/', views.lista_estoque, name='lista_estoque'),
+    path('estoque/adicionar/', views.adicionar_produto, name='adicionar_produto'),
 ]
